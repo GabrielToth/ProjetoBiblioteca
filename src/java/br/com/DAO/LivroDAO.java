aef2c92fc4a3943d4859dd351a63c0eff17f66a7
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2022-10-03 21:36:43 -0300

    2

--- src/java/br/com/DAO/LivroDAO.java
+++ src/java/br/com/DAO/LivroDAO.java
@@ -3,12 +3,15 @@ package br.com.DAO;
 import br.com.DTO.LivroDTO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class LivroDAO {
     Connection con;
     PreparedStatement pstm;
+    ResultSet rs;
+    ArrayList<LivroDTO> lista = new ArrayList<>();
     
     public void CadastrarLivro(LivroDTO objLivroDTO) throws ClassNotFoundException, SQLException {
         String sql = "INSERT INTO livro (nome_livro) VALUES (?)";
@@ -25,7 +28,29 @@ public class LivroDAO {
         }
     }
     
-    public ArrayList<LivroDTO> PesquisarLivro() {}
+    public ArrayList<LivroDTO> PesquisarLivro() throws ClassNotFoundException {
+        String sql = "select * from livro";
+        con = new ConexaoDAO().conexaoDB();
+        
+        try {
+            pstm = con.prepareStatement(sql);
+            rs = pstm.executeQuery(sql);
+            
+            while (rs.next()) {
+                LivroDTO objLivroDTO = new LivroDTO();
+                objLivroDTO.setId_livro(rs.getInt("id_livro"));
+                objLivroDTO.setNome_livro(rs.getString("nome_livro"));
+                
+                lista.add(objLivroDTO);
+                
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("Erro de listar em consulta DB");
+        }
+        
+        return lista;
+    }
     
     
 }
---
+++ web/Livro/listarLivro.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : listarLivro
+    Created on : 3 de out. de 2022, 21:36:32
+    Author     : Ramon
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
